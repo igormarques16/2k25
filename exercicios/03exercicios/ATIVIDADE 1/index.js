1a1afeb7f8ea14625869cae43fb2f5edd6ccf878
igormarques16 <i.marques@iesb.edu.br>
2025-03-12 23:06:06 -0300

    ATIVIDADE 3

---
+++ exercicios/03exercicios/ATIVIDADE 1/index.js
@@ -0,0 +1,5 @@
+import { soma, subtracao, multiplicacao, divisao } from './calculadora.js'; 
+console.log("Soma: ", soma(10, 5)); 
+console.log("Subtração: ", subtracao(10, 5)); 
+console.log("Multiplicação: ", multiplicacao(10, 5)); 
+console.log("Divisão: ", divisao(10, 5)); 
