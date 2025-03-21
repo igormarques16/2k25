259c4ba2f6e577c165b72679432844fdbbc5f91c
igormarques16 <i.marques@iesb.edu.br>
2025-03-20 21:23:18 -0300

    Aula

---
+++ 05-fundamentos-react-native/componentes/PrimeiroComponente.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import {}  from 'react-native'
+import {Text, View}  from 'react-native'
+
+//função componente
+
+export default function PrimeiroComponente(){
+
+    //retorno com jsx
+    return (<View>
+        <Text>
+            Meu Primeiro Componente
+        </Text>
+
+    </View>
+    )
+}
+
